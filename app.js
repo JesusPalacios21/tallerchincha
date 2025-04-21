65cc7de1a7e3784597c005033ce270bcf59d7d68
Jesus Eduardo Yataco Palacios <jesuseduardoyatacopalacios339@gmail.com>
2025-04-21 17:41:28 -0500

    En proceso

---
+++ app.js
@@ -0,0 +1,25 @@
+const express = require('express')
+const bodyParser = require('body-parser')
+const path = require('path')
+
+const rutaVehiculo =require('./routes/vehiculo')
+//const rutaMarca = require('./routes/marca')
+
+const app = express();
+const PORT = process.env.PORT || 3000
+
+//Configuracion "middleware" => Capa de comunicacion
+
+app.use(bodyParser.urlencoded({extended: true}))
+app.use(bodyParser.json())
+app.use(express.static(path.join(__dirname, 'public')))
+
+//Configuracion de las rutas
+app.use('/', rutaVehiculo);         //Principal
+//app.use('/api/marcas', rutaMarca)   //Suministrar datos
+
+//Servidor WEB
+app.listen(PORT, () => {
+  console.log(`Servidor iniciado en http://localhost:${PORT}`)
+});
+
---
+++ routes/vehiculo.js
@@ -0,0 +1,10 @@
+const express = require('express')
+const router = express.Router()
+const db = require('../config/database')
+
+router.get('/', async (req,res) => {
+  const [vehiculos] = await db.query(`SELECT * FROM vehiculos`)
+  res.send(vehiculos)
+});
+
+module.exports = router
